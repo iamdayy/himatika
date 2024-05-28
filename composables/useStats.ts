e36379f82d4a9aa46044aaa1db58e333fca132aa
iamdayy <108299143+iamdayy@users.noreply.github.com>
2024-05-28 11:17:47 +0700

    get percentage only 2 decimal

--- composables/useStats.ts
+++ composables/useStats.ts
@@ -1,29 +1,38 @@
-import type { IEvent, IProject } from "~/types"
-
 export const useStats = () => {
-    const { eventsCanMeRegistered, eventsMe } = useEvents();
-    const { ProjectsCanMeRegistered, projectsMe } = useProjects();
-    const EventPercentage = computed<number>(() => {
-        return (eventsMe.value.length! / eventsCanMeRegistered.value?.length!) * 100;
-    })
-    const ProjectPercentage = computed<number>(() => {
-        return (projectsMe.value.length! / ProjectsCanMeRegistered.value?.length!) * 100;
-    })
-    const all = computed<number>(() => {
-        return eventsMe.value.length! + projectsMe.value.length!;
-    });
-    const allCanMeRegister = computed<number>(() => {
-        return eventsCanMeRegistered.value?.length! + ProjectsCanMeRegistered.value?.length!;
-    });
-    const allPercentage = computed<number>(() => {
-        return (all.value / allCanMeRegister.value) * 100
-    })
-    return {
-        eventsMe,
-        projectsMe,
-        EventPercentage,
-        ProjectPercentage,
-        all,
-        allPercentage
-    }
-}
+  function calc(num: number) {
+    var with2Decimals = num.toString().match(/^-?\d+(?:\.\d{0,2})?/)![0];
+    return parseFloat(with2Decimals);
+  }
+  const { eventsCanMeRegistered, eventsMe } = useEvents();
+  const { ProjectsCanMeRegistered, projectsMe } = useProjects();
+  const EventPercentage = computed<number>(() => {
+    return calc(
+      (eventsMe.value.length! / eventsCanMeRegistered.value?.length!) * 100
+    );
+  });
+  const ProjectPercentage = computed<number>(() => {
+    return calc(
+      (projectsMe.value.length! / ProjectsCanMeRegistered.value?.length!) * 100
+    );
+  });
+  const all = computed<number>(() => {
+    return eventsMe.value.length! + projectsMe.value.length!;
+  });
+  const allCanMeRegister = computed<number>(() => {
+    return (
+      eventsCanMeRegistered.value?.length! +
+      ProjectsCanMeRegistered.value?.length!
+    );
+  });
+  const allPercentage = computed<number>(() => {
+    return calc((all.value / allCanMeRegister.value) * 100);
+  });
+  return {
+    eventsMe,
+    projectsMe,
+    EventPercentage,
+    ProjectPercentage,
+    all,
+    allPercentage,
+  };
+};
