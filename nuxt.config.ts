ff686c68fb9976a5ecc520b32741a2129e1a6bbc
iamdayy <108299143+iamdayy@users.noreply.github.com>
2025-09-30 00:33:05 +0700

    set preset to vercel-edge

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   nitro: {
-    preset: "vercel",
+    preset: "vercel-edge",
   },
   devtools: { enabled: true },
   runtimeConfig: {
