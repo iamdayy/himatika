aa7a3afa28f5aa6163f9fb31e714cde540df03ee
iamdayy <108299143+iamdayy@users.noreply.github.com>
2025-10-03 19:13:48 +0700

    build as node

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   nitro: {
-    preset: "vercel",
+    // preset: "vercel",
     // output: {
     //   dir: "../../.vercel/output", // Example for an app in 'apps/my-app'
     // },
