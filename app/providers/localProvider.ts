84c2f7f84048978bfa3831f1d9d4d6d4de6de0d8
iamdayy <108299143+iamdayy@users.noreply.github.com>
2025-10-04 22:51:33 +0700

    localProvider fixing bug

--- app/providers/localProvider.ts
+++ app/providers/localProvider.ts
@@ -1,21 +1,10 @@
 import { createOperationsGenerator } from "#image";
 import type { ProviderGetImage } from "@nuxt/image";
-import { joinURL } from "ufo";
 
 const operationsGenerator = createOperationsGenerator();
 
-export const getImage: ProviderGetImage = (
-  src,
-  { modifiers = {}, baseURL } = {}
-) => {
-  if (!baseURL) {
-    // also support runtime config
-    baseURL = useRuntimeConfig().storageDir;
-  }
-
-  const operations = operationsGenerator(modifiers);
-
+export const getImage: ProviderGetImage = (src) => {
   return {
-    url: joinURL(baseURL, src + (operations ? "?" + operations : "")),
+    url: src,
   };
 };
