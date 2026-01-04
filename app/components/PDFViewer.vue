a1fc2ac9914366d35126c90b86096559f1f55be9
iamdayy <108299143+iamdayy@users.noreply.github.com>
2026-01-04 08:03:23 +0700

    fix: update pdfjs-dist to version 5.4.149 across components and configuration

--- app/components/PDFViewer.vue
+++ app/components/PDFViewer.vue
@@ -108,7 +108,7 @@
 import * as pdfjsLib from 'pdfjs-dist';
 import type { PDFDocumentProxy } from 'pdfjs-dist/types/src/display/api';
 import { ref, watch } from 'vue';
-pdfjsLib.GlobalWorkerOptions.workerSrc = "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/5.4.530/pdf.worker.min.mjs";
+pdfjsLib.GlobalWorkerOptions.workerSrc = "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/5.4.149/pdf.worker.min.mjs";
 
 const props = defineProps({
     pdfUrl: {
