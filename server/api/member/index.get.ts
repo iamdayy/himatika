5f94f37e5abb0fe73858ec3c1295296953ea91a9
iamdayy <108299143+iamdayy@users.noreply.github.com>
2026-01-13 07:34:24 +0700

    fix point log not registered

--- server/api/member/index.get.ts
+++ server/api/member/index.get.ts
@@ -2,6 +2,7 @@ import { SortOrder } from "mongoose";
 import { AspirationModel } from "~~/server/models/AspirationModel";
 import { MemberModel } from "~~/server/models/MemberModel";
 import OrganizerModel from "~~/server/models/OrganizerModel";
+import { PointModel } from "~~/server/models/PointModel";
 import { IAgenda, IMember, IOrganizer, IProject } from "~~/types";
 import { IReqMemberQuery } from "~~/types/IRequestPost";
 import { IMemberResponse } from "~~/types/IResponse";
@@ -207,6 +208,7 @@ export default defineEventHandler(async (event): Promise<IMemberResponse> => {
       })
       .populate({
         path: "manualPoints",
+        model: PointModel,
       })
       .sort(sortOpt)
       .skip((Number(page) - 1) * Number(perPage))
