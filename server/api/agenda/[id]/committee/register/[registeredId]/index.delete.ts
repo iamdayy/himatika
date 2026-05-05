dcb65d1d8d661ac02033a42ef388bd8b692b26f5
Dayan Sauqy <iamdayy14@gmail.com>
2026-05-05 15:33:42 +0700

    fix: improve error handling for committee deletion by updating status messages

--- server/api/agenda/[id]/committee/register/[registeredId]/index.delete.ts
+++ server/api/agenda/[id]/committee/register/[registeredId]/index.delete.ts
@@ -39,7 +39,7 @@ export default defineEventHandler(async (event): Promise<IResponse> => {
     if (!agenda.committees) {
       throw createError({
         statusCode: 404,
-        statusMessage: "Committee not found",
+        statusMessage: "No committees found for this agenda",
       });
     }
     const indexCommittee = agenda.committees.findIndex(
@@ -49,6 +49,7 @@ export default defineEventHandler(async (event): Promise<IResponse> => {
       throw createError({
         statusCode: 404,
         statusMessage: "Committee not found",
+        message: "index of committee not found",
       });
     }
 
